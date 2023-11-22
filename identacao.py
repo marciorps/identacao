54e2c24955237eb415b1046e59fb8cb8f708d2e4
marciorps <marciorps@yahoo.com,.br>
2023-11-22 10:24:23 -0300

    identacao

---
+++ identacao.py
@@ -0,0 +1,34 @@
+if 10 > 5:
+    print('10 é maior que 5')
+
+class BemVindo():
+    print('Bem vindo')
+
+oi = BemVindo()
+
+print('ola')
+
+15 + 20 - 5
+
+
+#Desafio
+
+import time
+
+
+def PensarPor10Segundos():
+    print('pensando')
+    time.sleep(10)
+    print('Lembrei!')
+
+
+if 10 > 5:    
+    print('10 é maior que 5')
+
+class BemVindo():
+      def __init__(self):
+        print('Bem vindo')
+
+oi = BemVindo()
+
+
